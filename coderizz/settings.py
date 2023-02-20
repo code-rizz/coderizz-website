87e60b12c92a1ef450c828fe8201668c15371549
code-rizz <124332116+code-rizz@users.noreply.github.com>
2023-02-21 01:01:57 +0530

    Update settings.py

--- coderizz/settings.py
+++ coderizz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$q*(13tdfp-0^b5zs8fr5343kq8i2_elgnt_m22@%tg5l91kp^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
